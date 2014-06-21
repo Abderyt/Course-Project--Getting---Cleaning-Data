19ea4d09280ad601635ede26a0a9277478cbdcfe
IllWind <sublett@bresnan.net>
2014-06-20 19:01:16 -0600

    Files added

---
+++ README.Rmd
@@ -0,0 +1,27 @@
+## README.md
+
+The code, run_analysis.R, is extensively commented.
+
+The code is designed to run in the folder/directory where the provided data is located.
+
+The data files are all text files.  Unix and Unix-like systems have a rich set of mature text manipulation and managements tools. Two of those tools, sed and cat, are used here to clean and manipulate the constituent data files.  Those tools are called via the R system() command. On windows machines the cygwin set provides similar capabilities.
+
+The initial file cleaning steps are done to remove undesired, inconsistent spacing in the the data files and to increase the human readability of the feature.txt file. The modified features.txt file is saved as colNames.txt. The two data files are then concatenated to form a single file for the data, aggregateData.txt. The y_train.txt and y_test.txt file are concatenated to produce the acivities.txt file. The subject_train.txt and subject_test.txt files are concatenated to produce the subject.txt file. These steps facilitate the subsequent reading and manipulation of the files using the data.table library. The use of the data.table library is highly desirable due to the increased versatility of the data.table functions over those of the data frame. As side advantages the data.table functions improve speed of execution and generally use less memory.
+
+All produced files where checked to insure complete data.  No missing data was found. The colNames.txt was found to have three sets of duplicate file names. Correcting the duplicates was left to process using R functions.
+
+The aggregateData.txt, activities.txt, subject.txt and colNames.txt are then read as data tables using the data.table fread command.
+
+The colName.txt file is then added to the aggregateData.txt to name the data columns.  The duplicateNameFix function, a modification of a file referenced in the discussion section of a previous Data Analysis course, is used to find and fix the noted duplicate file names. The newNames file is then set to be the column names of the aggregateDT data table.
+
+The activities file is then modified to replace the numerical activity entries with man readable text entries. Then the activities and subjects are each added to the aggregrateDT file to become the 562 and 563 variables in each record observation.
+
+Next the aggregateDT is subsetted to obtain a data table which contains the columns that are the mean and std of each measurement. Grep is used to get the column numbers of the mean and std columns.  These are used as an index to subset the aggregateDT data table; this is named the extractDT data table.
+
+A double key,subjects and activities, is then set on the extract DT data table. The grouping and use of functions in the column selection to produce means for each activity for each subject. Here is the primary reason the previous work was done to allow the data.table library to be used.  One command produces the desired clean, tidy data table, meanDT. It has 180 records, 6 activity records for each of the 30 subjects.  This is saved to disk as a data.table.
+
+The last 7 lines of code are used to demonstrate an example of the contents of the meanDT data table. The meanDT data.tables is first removed from the work space, then fread from the saved file back into the work space. Several rows and columns of each are then read to stdout to show the results of having generated the data.table.
+
+
+
+
---
+++ run_analysis.R
@@ -0,0 +1,150 @@
+
+library(data.table)
+
+# This is a 2 line set of regular expressions that are 
+#      passed to sed to remove the 2 spaces at the beginning of
+#      the data sets and then changes all column spacing to a single space.
+#      This action facilitates the reading to the files into a data table.
+write("/^[ ]*/s///\n/  /s// /g","dataCmds.txt")
+
+# This line cleans the training data
+system("sed -f dataCmds.txt train/X_train.txt > trainData.txt")
+
+# This line cleans the testing data
+system("sed -f dataCmds.txt test/X_test.txt > testData.txt")
+
+# This line concatenates the two data files into a single file
+system("cat trainData.txt testData.txt > aggregateData.txt")
+
+# This is a 7 line set of regular expressions that are passed to
+#     sed to do the following to the features.txt file:
+#		  Remove empty parenthesis
+#         Replace commas with periods
+#         Replace dashes with periods
+#         Replace the fBody abbreviation at the beginning of names with the string freqBody
+#         Replace the tBody abbreviation at the beginning of names with the string timeBody
+#         Replace the tGravity abbreviation at the beginning of names with the string timeGravity
+#         Remove a stray right paraenthesis in line 556	 
+write("/([ ]*)/s///g\n/,/s//./g\n/-/s//./g\n/fBody/s//freqBody/\n/tBody/s//timeBody/\n/tGravity/s//timeGravity/\n556 s/n)./n./","featureCmds.txt")
+
+# This executes the cleaning of the feature.txt file and produces a the colNames.txt file
+system("sed -f featureCmds.txt features.txt > colNames.txt")
+
+# These two lines concatenate the two subject and activities files to single 10299
+#     line subject.txt and activities.txt files
+system("cat train/subject_train.txt test/subject_test.txt > subjects.txt")
+system("cat train/y_train.txt test/y_test.txt > activities.txt")
+
+# Now get and read the single aggregated data file.
+aggregateDT<-fread("aggregateData.txt")
+
+# Get and read the activities file
+activities<-fread("activities.txt")
+
+# Get and read the subject file
+subjects<-fread("subjects.txt")
+
+# Get the column names.
+colNamesDT<-fread("colNames.txt")
+colNames<-colNamesDT[,V2]
+
+# Set the column names for the aggregate data file
+setnames(aggregateDT,colNames)
+
+# A function to find and fix duplicate column names in a data frame 
+#      Since a data.table is classed as both a data.table and a date.frame
+#      this code is appropriate.
+duplicateNamesFix<-function(dataFrame){
+  currentNames<-names(dataFrame)
+  currentNamesDuplicates<-names(table(currentNames)[table(currentNames)>1])
+  nameVector<-character()
+  for(i in seq_along(currentNames)){
+    nameVector[i]<-currentNames[i];
+    if( !(nameVector[i] %in% currentNamesDuplicates) ){
+      names(nameVector)[i]<-currentNames[i];
+    } else {
+      j<-1;
+      repeat {
+        newName<-paste(currentNames[i],letters[j],sep=".");
+        if(is.na(nameVector[newName])) break;
+        j<-j+1;
+      }
+      names(nameVector)[i]<-newName;
+    }
+  }
+  names(nameVector);
+}
+
+# Find and fix duplicates in the column name file and
+#     then set the names for the data file
+newNames<-duplicateNamesFix(aggregateDT)
+setnames(aggregateDT,newNames)
+
+# The activities_labels.txt file has this sequence of activities associated
+#     with the numbers 1 through 6. The formatting of the file is not
+#     consistent with the the formatting used in the rest of this
+#     data table.  Rather than using a several lines of code to correct the 
+#     formatting, it is more straight forward to simply use the following
+#     to get the correctly formatted activity vector.
+activityVector<-c("Walking","WalkingUpStairs","WalkingDownStairs","Sitting","Standing","Laying")
+for(i in 1:6) {
+	activities[activities==i]<-activityVector[i]
+}
+
+# The following 2 lines add the activities and subjects columns to the table;
+#     it now has 563 columns.  The table is clean and ready for analytic work.
+aggregateDT[,activities:=activities]
+aggregateDT[,subjects:=subjects]
+colNames<-names(aggregateDT)
+
+# Gets the column numbers of the columns that contain the mean, std, activities, and subjects.
+indexExtractColumnsDT<-sort(c(grep("\\bmean\\b",colNames),grep("\\bstd\\b",colNames),grep("^activities|subjects",colNames)))
+
+# Subsets the aggregateDT to the extractDT the contains means and std columns.
+extractDT<-aggregateDT[,indexExtractColumnsDT,with=FALSE]
+
+# Sets a double key on the extractDT data table
+setkey(extractDT,subjects,activities)
+
+# The key is then used to extract the mean of each variable for each subject,
+#     thus producing the desired second, independent tidy data table
+meanDT<-extractDT[,lapply(.SD,mean),by=list(subjects,activities)]
+
+# Modify the column names to reflect the variable contents
+meanDTColNames<-names(meanDT)
+meanDTColNames<-sub('^',"Mean\\.",meanDTColNames)
+meanDTColNames<-sub('^Mean\\.subjects',"subjects",meanDTColNames)
+meanDTColNames<-sub('^Mean\\.activities',"activities",meanDTColNames)
+
+# Set the modified column names
+setnames(meanDT,meanDTColNames)
+
+# Now write the table to a file.  The meanDT data.table is the desired
+#     ovjective of this code.
+write.table(meanDT,"meanDT.txt",row.names=FALSE)
+
+# The following is Demonstration code.
+
+workSpace<-ls()
+if("meanDT" %in% workSpace){rm(meanDT,workSpace)}
+colNums<-c(1:3,68)
+rowNums<-c(1:12,180)
+meanDT<-fread("meanDT.txt")
+dim(meanDT)
+meanDT[rowNums,colNums,with=FALSE]
+
+#  This produces the first 12 and the last row of columns 1 through 3 and 68, the last column
+#     subjects        activities Mean.timeBodyAcc.mean.X Mean.freqBodyBodyGyroJerkMag.std
+#  1:        1            Laying               0.2215982                       -0.9326607
+#  2:        1           Sitting               0.2612376                       -0.9870496
+#  3:        1          Standing               0.2789176                       -0.9946711
+#  4:        1           Walking               0.2773308                       -0.3816019
+#  5:        1 WalkingDownStairs               0.2891883                       -0.3919199
+#  6:        1   WalkingUpStairs               0.2554617                       -0.6939305
+#  7:        2            Laying               0.2813734                       -0.9894927
+#  8:        2           Sitting               0.2770874                       -0.9896329
+#  9:        2          Standing               0.2779115                       -0.9777543
+# 10:        2           Walking               0.2764266                       -0.5581046
+# 11:        2 WalkingDownStairs               0.2776153                       -0.3436990
+# 12:        2   WalkingUpStairs               0.2471648                       -0.6218202
+# 13:       30   WalkingUpStairs               0.2714156                       -0.7913494
